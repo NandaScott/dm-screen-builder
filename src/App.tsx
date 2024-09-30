623b69faf33fd608fccf1cd19cb8d50195f7bbef
NandaScott <nanda1123@gmail.com>
2024-09-30 17:29:31 -0400

    Added react-to-print and adjusted combat page for single page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { useRef } from 'react';
 import Combat from './pages/combat';
+import { useReactToPrint } from 'react-to-print';
 
 export default function App() {
-  return <Combat />;
+  const contentRef = useRef<HTMLDivElement>(null);
+  const reactToPrintFn = useReactToPrint({ contentRef }) as any;
+
+  return (
+    <div>
+      <button onClick={reactToPrintFn}>Print</button>
+      <Combat ref={contentRef} />
+    </div>
+  );
 }
--- src/pages/combat.tsx
+++ src/pages/combat.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import ActionsInCombat from '../tables/actions-in-combat';
 import CombatResources from '../tables/combat-resources';
 import CombatSmallNotes from '../tables/combat-small-notes';
@@ -6,32 +7,36 @@ import DescribingDamage from '../tables/describing-the-effects-of-damage';
 import TwoWeaponFighting from '../tables/two-weapon-fighting';
 import UnseenAttackers from '../tables/unseen-attackers';
 
-export default function Combat() {
-  return (
-    <div className='flex flex-col items-center p-4 gap-4 font-sans'>
-      <h1 className='text-3xl font-bold'>Combat</h1>
-      <div className='flex flex-col gap-4 max-w-screen-md' id='page'>
-        <div className='flex gap-4'>
-          <div className='flex flex-col gap-4 w-1/2'>
-            <ActionsInCombat />
-          </div>
-          <div className='flex flex-col gap-4 w-1/2'>
-            <CombatResources />
-            <CombatSmallNotes />
-            <CoverInCombat />
-          </div>
+const Combat = forwardRef<HTMLDivElement>((_, ref) => (
+  <div ref={ref} className='flex flex-col items-center p-4 gap-4 font-sans'>
+    <h1 className='text-3xl font-bold'>Combat</h1>
+    <div className='flex flex-col gap-4 max-w-screen-md' id='page'>
+      <div className='flex gap-4'>
+        <div className='flex flex-col gap-4 w-1/2'>
+          <ActionsInCombat />
         </div>
+        <div className='flex flex-col gap-4 w-1/2'>
+          <CombatResources />
+          <CombatSmallNotes />
+          <CoverInCombat />
+        </div>
+      </div>
 
+      <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
           <div className='flex flex-col gap-4 w-1/2'>
             <TwoWeaponFighting />
-            <UnseenAttackers />
           </div>
           <div className='flex flex-col gap-4 w-1/2'>
             <DescribingDamage />
           </div>
         </div>
+        <div className='flex'>
+          <UnseenAttackers />
+        </div>
       </div>
     </div>
-  );
-}
+  </div>
+));
+
+export default Combat;
